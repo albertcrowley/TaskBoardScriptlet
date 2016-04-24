59aaab6bdc24efb8da0e005b15f4ad1b6fee4b08
Al Crowley <albert.crowley@tcg.com>
2016-04-24 14:42:13 -0400

    Show the same color for each assignee

--- scriptlet.js
+++ scriptlet.js
@@ -1,5 +1,12 @@
 jQuery("document").ready(function () {
     var table = jQuery("#issuetable").first();
+    var people = [];
+    categories = ['NotReady', 'ReadyForDev', 'PeerReview', 'QA', 'Done'];
+    series = [];
+    ASSIGNED = 0;
+    UNASSIGNED = 1;
+    COLORS = [[205, 82, 217],[109, 179, 50],[118, 108, 204],[196, 146, 58],[185, 81, 166],[211, 73, 115],[77, 162, 150],[213, 75, 51],[117, 159, 209],[151, 81, 55],[77, 87, 122],[76, 94, 43],[201, 138, 176],[126, 59, 93],[205, 82, 217],[109, 179, 50],[118, 108, 204],[196, 146, 58],[185, 81, 166],[103, 163, 88],[211, 73, 115],[77, 162, 150],[213, 75, 51],[117, 159, 209],[151, 81, 55],[77, 87, 122],[76, 94, 43],[201, 138, 176],[126, 59, 93]  ];
+
 
     rows = jQuery(table).find("tr.issuerow");
 
@@ -11,6 +18,7 @@ jQuery("document").ready(function () {
         rowdata.status = jQuery(row).find(".status").text().trim();
         rowdata.assignee = jQuery(row).find(".assignee").text().trim();
         rowdata.timeoriginalestimate = jQuery(row).find(".timeoriginalestimate").text().trim();
+        rowdata.issuekey = jQuery(row).find(".issuekey").text().trim();
         report.push(rowdata);
     });
 
@@ -21,10 +29,6 @@ jQuery("document").ready(function () {
     buckets.QA = [];
     buckets.Done = [];
 
-    categories = ['NotReady', 'ReadyForDev', 'PeerReview', 'QA', 'Done'];
-    series = [{ name: 'Assigned', data: [0, 0, 0, 0, 0] }, { name: 'Unassigned', data: [0, 0, 0, 0, 0] }];
-    ASSIGNED = 0;
-    UNASSIGNED = 1;
 
 
     stati = [];
@@ -33,57 +37,70 @@ jQuery("document").ready(function () {
         stati.push(row.status);
 
 
-        if (!checkForSpecialCase(row, buckets)) {  // only do the switch if this is not a special case
+        // check to see if any special conditions will set this row to a different category than the status would suggest
+
+        row = checkForSpecialCase(row);
+
+
             switch (row.status) {
                 case "Open":
                 case "Ready for Development":
                 case "Development in Progress":
-                    addToBucket(row, buckets, "ReadyForDev", series);
+                    addToBucket(row, buckets, "ReadyForDev", series, people);
                     break;
                 case "Ready for Code Review":
                 case "Code Review in Progress":
-                    addToBucket(row, buckets, "PeerReview", series);
+                    addToBucket(row, buckets, "PeerReview", series, people);
                     break;
                 case "Ready for QA Review":
                 case "QA Review in Progress":
-                    addToBucket(row, buckets, "QA", series);
+                    addToBucket(row, buckets, "QA", series, people);
                     break;
                 case "Ready for Security Audit":
-                    addToBucket(row, buckets, "Done", series);
+                    addToBucket(row, buckets, "Done", series, people);
                     buckets.done.push(row);
                     break;
                 default:
-                    addToBucket(row, buckets, "NotReady", series);
+                    addToBucket(row, buckets, "NotReady", series, people);
                     break;
             }
-        }
     });
 
 
+
+    console.log(categories);
+    console.log(series);
+
+    console.log(people);
 
 
     drawChart(categories, series);
 
 });
 
-function addToBucket(row, buckets, category, series) {
-    buckets[category].push(row);
-    if (row.assignee.toLowerCase() == 'unassigned') {
-        series[UNASSIGNED].data[_.indexOf(categories, category)] += convertTime(row.timeoriginalestimate);
-    } else {
-        series[ASSIGNED].data[_.indexOf(categories, category)] += convertTime(row.timeoriginalestimate);
+function addToBucket(row, buckets, category, series, people) {
+    if (_.indexOf(people, row.assignee) == -1) {
+        people.push(row.assignee);
     }
+    var colorTripple = COLORS[_.indexOf(people, row.assignee)];
+    var colorString = rgbToHex(colorTripple[0], colorTripple[1], colorTripple[2]);
+    buckets[category].push(row);
+    var stack =  (row.assignee.toLowerCase() == 'unassigned') ? "unassigned" : "assigned";
+    var data = [0, 0, 0, 0, 0];
+
+    data[_.indexOf(categories, category)] += convertTime(row.timeoriginalestimate);
+    series.push({ name: row.summary, data: data, stack: stack, assignee: row.assignee, status: row.status, issuekey: row.issuekey, color: colorString });
 };
 
 
-function checkForSpecialCase(row, buckets) {
+function checkForSpecialCase(row) {
     var who = row.assignee.toLowerCase();
     if (who.indexOf("jose") > -1 || who.indexOf("tsitsi") > -1) {
-        // In general, this one is not ready for work no matter what the state because it's assigned to the RA team
-        console.log(row);
-        buckets.NotReady.push(row);
-            series[ASSIGNED].data[_.indexOf(categories, "NotReady")] += convertTime(row.timeoriginalestimate);
+        console.log("fixing category for oddball element");
+        row.status = "Assigned to a Non-Developer";
     }
+
+    return row;
 
 }
 
@@ -98,7 +115,7 @@ function drawChart(categories, series) {
                 type: 'column'
             },
             title: {
-                text: 'Stacked column chart'
+                text: 'Agile Task Board'
             },
             xAxis: {
                 categories: categories
@@ -106,7 +123,7 @@ function drawChart(categories, series) {
             yAxis: {
                 min: 0,
                 title: {
-                    text: 'Total fruit consumption'
+                    text: 'Number of Hours'
                 },
                 stackLabels: {
                     enabled: true,
@@ -116,20 +133,15 @@ function drawChart(categories, series) {
                     }
                 }
             },
-            legend: {
-                align: 'right',
-                x: -30,
-                verticalAlign: 'top',
-                y: 25,
-                floating: true,
-                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-                borderColor: '#CCC',
-                borderWidth: 1,
-                shadow: false
-            },
+            legend: { enabled: false},
             tooltip: {
                 headerFormat: '<b>{point.x}</b><br/>',
-                pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+                formatter:  function() {
+                    
+                    console.log(series);
+                    
+                    return "i: " + this.series._i + "<br/>Title:<b>" + this.series.name + "</b><br/>Ticket#: " + series[this.series._i].issuekey + "<br/>Assigned: " + series[this.series._i].assignee + "<br>Status: " + series[this.series._i].status;
+                }
             },
             plotOptions: {
                 column: {
@@ -146,4 +158,14 @@ function drawChart(categories, series) {
             series: series
         });
 
+}
+
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
